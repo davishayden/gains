ad24750f1c58a4b4b0baa6e32ab496cbbd78a8b3
davishayden <1hayden.davis@gmail.com>
2016-10-18 11:30:53 -0400

    Added lifts app

--- urban_train/urls.py
+++ urban_train/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^lifts/', include('lifts.urls')),
     url(r'^admin/', admin.site.urls),
 ]
