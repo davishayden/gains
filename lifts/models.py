293ff4b9cd479a8622b0d9a74c901f66556f9c94
davishayden <1hayden.davis@gmail.com>
2016-10-18 11:44:30 -0400

    Add lifts model

--- lifts/models.py
+++ lifts/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 
 # Create your models here.
+class Lift(models.Model):
+	name = models.CharField(max_length=200)
+	created_at = models.DateTimeField('created at')
+	m_group = models.CharField(max_length=200)
+	def __str__(self):
+		return self.name
