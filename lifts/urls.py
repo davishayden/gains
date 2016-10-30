8a9855537e8dd1a3cd5b938fa71bef79c40af1a2
davishayden <1hayden.davis@gmail.com>
2016-10-30 17:24:39 -0400

    changes

--- lifts/urls.py
+++ lifts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
     url(r'^(?P<workout_id>[0-9]+)/$', views.workoutDetail, name='workoutDetail')
 ]
--- lifts/views.py
+++ lifts/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from models import WorkoutInstance
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 
 def index(request):
     latest_workout_list = WorkoutInstance.objects.order_by('-created_at')[:5]
@@ -12,3 +14,17 @@ def workoutDetail(request, workout_id):
     workout_details = WorkoutInstance.objects.get(id=workout_id)
     context = {'workout_details': workout_details}
     return render(request, 'lifts/workoutdetail.html', context)
+
+def login(request):
+    return HttpResponseRedirect("/lifts")
+
+    #username = request.POST['username']
+    #password = request.POST['password']
+    #user = authenticate(username=username, password=password)
+    #if user is not None:
+     #   login(request, user)
+        # Redirect to a success page.
+    #else:
+        # Return an 'invalid login' error message.
+    #user = authenticate(username="hayden", password="password")
+    #return render(request, 'lifts/index.html', user)
